aa2fe787fb42f159f887358ae9d64f6515e99298
2men23 <2men23andmor@gmail.com>
2017-09-20 18:35:17 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -115,6 +115,6 @@
         },));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript('https://rawgit.com/basicBot/custom/master/extension.js'), extend);
+    $.getScript('https://rawgit.com/basicBot/source/master/basicBot.js'), extend);
 
 }).call(this);
